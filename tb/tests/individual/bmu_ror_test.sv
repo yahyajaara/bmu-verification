730aa0b6f6612b41f4b6349a33864d32d48ea5d0
yahyajaara <yahyajaara1411@gmail.com>
2026-09-18 00:44:50 +0300

    add shift and masking individual tests

---
+++ tb/tests/individual/bmu_ror_test.sv
@@ -0,0 +1,25 @@
+
+class bmu_ror_test extends bmu_base_test;
+
+    `uvm_component_utils(bmu_ror_test)
+
+
+    function new(string name = "bmu_ror_test",uvm_component parent = null);
+        super.new(name, parent);
+    endfunction
+
+
+    // Build Phase
+    function void build_phase(uvm_phase phase);
+
+        // Replace the base sequence with the ror sequence
+        bmu_base_sequence::type_id::set_type_override(
+            bmu_ror_sequence::get_type()
+        );
+
+        super.build_phase(phase);
+
+    endfunction
+
+
+endclass
